30269171943bc6e9e431dedfc54419f97748c2cb
John Mark Harrell <hijohnmark@gmail.com>
2024-09-17 17:01:18 +0700

    complete anagram detector lab

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,13 @@
 # your code goes here!
+
+
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+        
+    def match(self, list):
+        new_list = []
+        for item in list:
+            if sorted([letter for letter in item]) == sorted([letter for letter in self.word]):
+                new_list.append(item)
+        return new_list
